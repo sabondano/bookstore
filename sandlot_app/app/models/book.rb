5d36b072597656ddb1271bfce71c047444be50f1
Sebastian Abondano <sabondano1@gmail.com>
2016-01-17 11:27:12 -0500

    Add #author_name to Book model

--- sandlot_app/app/models/book.rb
+++ sandlot_app/app/models/book.rb
@@ -6,4 +6,8 @@ class Book < ActiveRecord::Base
   has_many :book_format_types, through: :book_formats
 
   validates :title, :author_id, :publisher_id, presence: true
+
+  def author_name
+    "#{author.last_name}, #{author.first_name}"
+  end
 end
